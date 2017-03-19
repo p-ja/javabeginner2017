4d5c64934c3fc5214c872bcd0beaf543ddc2ec19
unknown <jankowski.piotr@gmail.com>
2017-03-20 00:17:28 +0100

    annotations

--- workshop/src/Sandbox.java
+++ workshop/src/Sandbox.java
@@ -1,4 +1,8 @@
+
+@Workshop("sandbox")
 public class Sandbox {
+
+    @Workshop(year = 2016)
     public static void main(String[] args) {
         float a = 1.0f;
         float b = 2.0f;
@@ -14,5 +18,31 @@ public class Sandbox {
         bitB <<= 2;
         System.out.println(bitB);
 
+        Sandbox sandbox = new Sandbox();
+        sandbox.oldOne();
+        sandbox.user1();
+        sandbox.user2();
+    }
+
+    @Deprecated
+    public void oldOne() {
+
+    }
+
+    @SuppressWarnings("deprecation")
+    public void user1() {
+        oldOne();
+    }
+
+    public void user2() {
+        oldOne();
+    }
+
+    public static <E> void printArray(E[] inputArray) {
+        // Display array elements
+        for (E element : inputArray) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
     }
 }
---
+++ workshop/src/Workshop.java
@@ -0,0 +1,14 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Workshop {
+    String value() default "unknown";
+    int year() default 2017;
+}
